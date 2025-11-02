dfd45dceefc40b1bebaf79975fc2e242c4f6b688
AJCordova <archonist252@gmail.com>
2025-11-02 16:51:42 +0000

    Init project.

---
+++ MiseTrack/MiseTrack/MiseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiseTrackApp.swift
+//  MiseTrack
+//
+//  Created by Jireh Cordova on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MiseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

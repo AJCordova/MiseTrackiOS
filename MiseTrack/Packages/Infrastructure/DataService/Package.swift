eba3fa633cfa910b5c8a6973421343a627869489
AJCordova <archonist252@gmail.com>
2025-11-22 17:36:00 +0000

    Create DataService package. Add Firebase dependency.

--- MiseTrack/Packages/Infrastructure/DataService/Package.swift
+++ MiseTrack/Packages/Infrastructure/DataService/Package.swift
@@ -4,34 +4,37 @@
 import PackageDescription
 
 let package = Package(
-    name: "SauceServices",
+    name: "DataService",
     platforms: [
         .iOS("18.4")
       ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SauceServices",
-            targets: ["SauceServices"]
+            name: "DataService",
+            targets: ["DataService"]
         ),
     ],
     dependencies: [
         .package(path: "../../Base/Models"),
-        .package(path: "../DataService")
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk",
+            from: "12.6.0"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SauceServices",
+            name: "DataService",
             dependencies: [
                 .product(name: "Models", package: "Models"),
-                .product(name: "DataService", package: "DataService"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
             ]
         ),
         .testTarget(
-            name: "SauceServicesTests",
-            dependencies: ["SauceServices"]
+            name: "DataServiceTests",
+            dependencies: ["DataService"]
         ),
     ]
 )
--- MiseTrack/Packages/Infrastructure/SauceServices/Sources/SauceServices/DataTransferObjects/SauceDTO.swift
+++ MiseTrack/Packages/Infrastructure/SauceServices/Sources/SauceServices/DataTransferObjects/SauceDTO.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import DataService
 import Models
 
 public struct SauceDTO: Codable, Identifiable {

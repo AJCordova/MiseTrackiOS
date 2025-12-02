ab5e67ac93a4c310586d7847c33a00efd009cfe4
AJCordova <archonist252@gmail.com>
2025-12-02 16:00:42 +0000

    Create service test.

--- MiseTrack/Packages/Features/SauceServices/Tests/SauceServicesTests/SauceServicesTests.swift
+++ MiseTrack/Packages/Features/SauceServices/Tests/SauceServicesTests/SauceServicesTests.swift
@@ -9,4 +9,12 @@ final class SauceServicesTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
+    
+    func testFetchSauces() async throws {
+        let mockRepo = MockSauceRepository()
+        let service = SauceService(repository: mockRepo)
+        
+        let sauces = try await service.getAllSauces()
+        XCTAssertFalse(sauces.isEmpty)
+    }
 }
